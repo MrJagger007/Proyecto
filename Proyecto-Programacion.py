d51f0c7068d12620d9a011c3f76341c445b26fc0
Ronny López <134113836+MrJagger007@users.noreply.github.com>
2023-06-12 16:37:12 -0600

    Update

--- Proyecto-Programacion.py
+++ Proyecto-Programacion.py
@@ -1,2 +1,17 @@
-Entrance1=input("Favor introduzca su nombre completo")
-Full_Name=Entrance1
+print ("Por favor seleccione una de las siguientes opciones: \n 1. Ingresar nueva compra \n 2. Salir ")
+
+#Proceso de selección por parte del cliente
+options= int( input ("Ingrese el número de la opción que desea"))
+
+#Fin de Módulo 1
+#Inicio de Módulo 2
+
+if options==1:
+    name= input("Por favor ingrese su nombre completo")
+    date= input("Por favor digíte la fecha en la que esta realizando la compra en el formato: Día/Mes/Año")
+
+    print ("Productos disponibles: \n1. Atún enlatado: 1200 \n2. Cartón de Leche: 1100 \n3. Botella de Vinagre: 1800 \n4. Botella de Vino: 15000 \n5. Coca Cola 2.5L : 2000 \n6. Agua Embotellada 1L : 700 \n7. Chocolate Snickers: 950 \n8. Cartón de Huevos: 2600 \n9. Tosty Mix: 1850 \n10. Plátano: 250 \n11. Gomitas Trululu: 750 \n12. Porción Queque Spoon: 3000 \n13. Bolsa de Arroz 1KG: 1950 \n14. Bolsa de Frijoles 1KG: 1450 \n15. Caja de Helado Dos Pinos: 3180 \n16. Paquete Galletas Oreo: 3600 \n17. Ensure Vainilla: 14500 \n18. Lata Red Bull: 1350 \n19. Chocolate M&M: 900 \n20. Paquete Avena: 1100 \n21. Ice Cubes Gum: 2700 \n22. Botella Cloro 800ML: 2100 \n23. Pasta Dental Colgate: 1900 \n24. Enjuage Bucal LISTERINE 600ML: 2400 \n 25.Pechuga de Pollo 1KG: 3300")
+
+
+
+
